77be8f844c5afe8cd7ce9b3f7756a0d4621e6b31
Hash_kitti <iJeddy5.0@gmail.com>
2023-05-09 09:06:55 +0100

    Removed unused function

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,12 +1,3 @@
-window.onload = () => {
-    fetch('http://api.giphy.com/v1/gifs/search?q=windy%2B&api_key=4lNmkmcSK5tc69Y1iXQWDNRzQdtKm88N&limit=5')
-  .then(response => response.json())
-  .then(response => {
-    document.querySelector("body").backgroundImage = response.data.url;
-  })
-  .catch(err => console.error(err));
-    
-}
 const getWeatherData = () => {
     const country = document.querySelector("#country").value
     fetch(`https://api.weatherapi.com/v1/current.json?key=b2ab521e6be34abab47233139230705&q=${country}`)
