e7dd0d10947e86a8bc3d15e3e05f6eaf56541924
hashkitti001 <iJeddy5.0@gmail.com>
2023-10-27 01:17:27 +0100

    Added actions to the routes for items in index.js

--- ExpressTask/inventoryapp/routes/index.js
+++ ExpressTask/inventoryapp/routes/index.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let router = express.Router();
 let category_controller = require("../controllers/categoryController")
+let item_controller = require("../controllers/itemController");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -9,7 +10,9 @@ router.get('/', function(req, res, next) {
 router.get('/newitem', (req, res, next) => {
   res.render('item_form');
 })
-router.get('/items',)
+router.get('/items', item_controller.category_create_get);
+router.get('/items/create', item_controller.category_create_get);
+router.post('/items/create', item_controller.category_create_post);
 //Category routes and methods
 router.get('/categories', category_controller.category_create_get)
 router.get('/categories/create', category_controller.category_create_get);
