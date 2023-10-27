454ebbf254d12854df894a9c169641767c06327c
hashkitti001 <iJeddy5.0@gmail.com>
2023-10-27 01:13:48 +0100

    Changed all instances of var declared variables to let

--- ExpressTask/inventoryapp/routes/index.js
+++ ExpressTask/inventoryapp/routes/index.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var router = express.Router();
-var category_controller = require("../controllers/categoryController")
+let express = require('express');
+let router = express.Router();
+let category_controller = require("../controllers/categoryController")
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+//Item routes and methods
 router.get('/newitem', (req, res, next) => {
   res.render('item_form');
 })
+router.get('/items',)
+//Category routes and methods
 router.get('/categories', category_controller.category_create_get)
 router.get('/categories/create', category_controller.category_create_get);
 router.post('/categories/create', category_controller.category_create_post);
