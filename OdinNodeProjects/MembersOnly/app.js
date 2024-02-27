5f262843b30dd9ca3e79f4060fa230c68af7801e
hashkitti001 <iJeddy5.0@gmail.com>
2024-02-27 21:16:12 +0100

    defined general and protected routes

--- OdinNodeProjects/MembersOnly/app.js
+++ OdinNodeProjects/MembersOnly/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -12,14 +12,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(logger('dev'));
+app.use(cookieParser());
+
+
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 
 
