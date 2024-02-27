af8b16bfc27aca098222f68420567da2284d442d
hashkitti001 <iJeddy5.0@gmail.com>
2024-02-27 16:21:42 +0100

    add styleseet for styling the views

--- OdinNodeProjects/MembersOnly/routes/index.js
+++ OdinNodeProjects/MembersOnly/routes/index.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+router.get('/signup', (req, res) => {
+  res.render('signup')
+})
 module.exports = router;
