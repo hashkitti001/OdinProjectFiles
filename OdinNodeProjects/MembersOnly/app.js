01b6d03e523b1bffc1dfb9a0c288cea02d4754ad
hashkitti001 <iJeddy5.0@gmail.com>
2024-03-01 12:36:30 +0100

    modifications to user schema

--- OdinNodeProjects/MembersOnly/app.js
+++ OdinNodeProjects/MembersOnly/app.js
@@ -4,24 +4,29 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-var authRouter = require('./routes/auth');
-
+//var authRouter = require('./routes/auth');
+var mongoose = require("mongoose")
+var session = require("express-session")
 var app = express();
-
+async function dbConn() {
+    await mongoose.connect("mongodb://localhost:27017")
+}
+dbConn()
+  .then(() => console.log("connected"))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(express.json());
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(cookieParser());
 
 
 
 app.use('/', indexRouter);
-app.use('/auth', authRouter);
 
 
 
--- OdinNodeProjects/MembersOnly/models/user.js
+++ OdinNodeProjects/MembersOnly/models/user.js
@@ -1,11 +1,47 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 const userSchema = new mongoose.Schema(
     {
-        fullname: {type: String, required: true},
-        username: {type: String, required: true},
-        password: {type:String, required: true},
-        membershipStatus: {type: Boolean}
+        fullname: {
+            type: String, 
+            required: true
+        },
+        username: {
+            type: String,
+             required: true,
+              unique: true
+            },
+        password: {
+            type:String,
+             required: true
+            },
+        membershipStatus: {
+            type: Boolean
+        },
+        role: {
+            type: String, 
+            enum: ['user', 'admin'],
+            default: 'user'
+        }
+    
     }
 )
-const User = mongoose.Model("users", userSchema);
+userSchema.pre('save', async function (next) {
+    //Gets the context of the User object
+    const user = this
+    //checks if the password
+    if(!user.isModified('password')) return next()
+    try{
+       const salt  = await bcrypt.genSalt()
+       user.password = await bcrypt.hash(user.password, salt)
+       next()
+    } catch (e) {
+        return next(e)
+    }
+})
+userSchema.methods.comparePassword = async function (password) {
+    //
+    return bcrypt.compare(password, this.password)
+}
+const User = mongoose.model("users", userSchema);
 module.exports = User;
